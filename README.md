18621731bd64c079858d65c6021f86e36e33dc79
Teddy <marinova.teddy@gmail.com>
2021-10-22 01:28:16 +0300

    updated

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # stuco-questions-bot
 This is a bot created to help manage the ACS Community Server in Discord.
+
+>Questions channel
+-anyone writes a question -> bot sends it anonymously
+-member of a specific role answers the question by !!answer [answer]
+-member of a specific role locks the question by !!lock [reason]
+
+question colors legend:
+    Grey - unanswered
+    Green - at least one answer
+    Red - locked and no answers
+    Orange - locked and at least one answer
--- bot.js
+++ bot.js
@@ -15,12 +15,12 @@ function formatTimestamp(timestamp) {
 client.on("message", async (message) => {
     if (message.author.bot) return;
     if (message.channel.id === "864878446647050257") { 
-        if (!message.content.startsWith("!!answer")) {
+        if (!message.content.startsWith("!!")) {
             //message is a question
             let question = new Discord.MessageEmbed()
-                .setColor("FF8C00")
+                .setColor("8c8c8c") // grey - submitted, not answered
                 .setTitle("Q: " + message.content)
-                .setDescription("Not answered yet... Please be patient.")
+                .setDescription("*Not answered yet... Please be patient.*")
                 .setFooter("Asked on " + formatTimestamp(message.createdTimestamp))
             message.channel.send(question);
             message.delete();
@@ -28,19 +28,35 @@ client.on("message", async (message) => {
         else {
             //message is an answer
             //!!should check if author is stuco member
-            if (message.reference != null) {
-                let answer = message.content.slice(9);
-                let name = "Anonymous";
-                let msg = await message.channel.messages.fetch(message.reference.messageID);
-                let embed = msg.embeds[0]
-                    .addFields(
-                        {name, value: "A: " + answer, inline: false}
-                    )
-                    .setColor("00EB00")
-                    .setDescription('')
-                    .setFooter("Asked on " + formatTimestamp(message.createdTimestamp) + "\nAnswered on " + formatTimestamp(message.createdTimestamp))
-                msg.edit(embed);
-                message.delete();
+            if (message.content.startsWith("!!answer")) {
+                if (message.reference != null) {
+                    let answer = message.content.slice(9);
+                    let name = "Anonymous";
+                    let msg = await message.channel.messages.fetch(message.reference.messageID);
+                    let embed = msg.embeds[0]
+                        .addField(name, "A: " + answer, false)
+                        .setColor("00cc00") // green - answered
+                        .setDescription('')
+                        .setFooter(msg.embeds[0].footer.text + "\nAnswered on " + formatTimestamp(message.createdTimestamp))
+                    msg.edit(embed);
+                    message.delete();
+                }
+            }
+            if (message.content.startsWith("!!lock")) {
+                if (message.reference != null) {
+                    let msg = await message.channel.messages.fetch(message.reference.messageID);
+                    let embed = msg.embeds[0]
+                    if (embed.fields.length === 0) {
+                        embed.setColor("cc0000"); //red - locked, no answer
+                    } else {
+                        embed.setColor("#e69500"); //orange - locked, answered
+                    }
+                    embed
+                        .setDescription("*This question has been locked.*")
+                        .setFooter(msg.embeds[0].footer.text + "\nLocked on " + formatTimestamp(message.createdTimestamp))
+                    msg.edit(embed);
+                    message.delete();
+                }
             }
         }
     }
